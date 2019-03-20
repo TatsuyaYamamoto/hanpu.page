bf059d3ec782f644ff990235bd8e1d3f8821e104
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2019-03-20 21:15:58 +0900

    wip, handle editable state

--- src/components/organisms/OmakeItemList.tsx
+++ src/components/organisms/OmakeItemList.tsx
@@ -7,6 +7,7 @@ import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelActions from "@material-ui/core/ExpansionPanelActions";
 import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
@@ -18,88 +19,169 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import SaveIcon from "@material-ui/icons/Save";
 import CancelIcon from "@material-ui/icons/Cancel";
+import AddIcon from "@material-ui/icons/Add";
+import { makeStyles } from "@material-ui/styles";
 
 const OmakeItemListItemRoot = styled.div`
   width: 100%;
 `;
 
-const fileTypes = [
+const CancelButton = (props: any) => {
+  const { ...others } = props;
+  return (
+    <Button size="small" color="primary" {...others}>
+      <CancelIcon />
+      Cancel
+    </Button>
+  );
+};
+
+const EditButton = (props: any) => {
+  const { ...others } = props;
+  return (
+    <Button size="small" color="primary" {...others}>
+      <EditIcon />
+      Edit
+    </Button>
+  );
+};
+
+const DeleteButton = (props: any) => {
+  const { ...others } = props;
+  return (
+    <Button size="small" color="primary" {...others}>
+      <DeleteIcon />
+      Delete
+    </Button>
+  );
+};
+
+const SaveButton = (props: any) => {
+  const { ...others } = props;
+  return (
+    <Button size="small" color="primary" {...others}>
+      <SaveIcon />
+      Save
+    </Button>
+  );
+};
+
+const AddItemButton = (props: any) => {
+  const { ...others } = props;
+  return (
+    <Button size="small" color="primary" {...others}>
+      <AddIcon />
+      ファイルを追加する
+    </Button>
+  );
+};
+
+const fileType = [
   {
-    value: "USD",
-    label: "$"
+    value: "wav_24bit",
+    label: "WAV 24bit"
   },
   {
-    value: "EUR",
-    label: "€"
-  },
-  {
-    value: "BTC",
-    label: "฿"
-  },
-  {
-    value: "JPY",
-    label: "¥"
+    value: "flac_16bit",
+    label: "FLAC 16bit"
   }
 ];
 
-const CancelButton = () => (
-  <Button size="small" color="primary">
-    <CancelIcon />
-    Cancel
-  </Button>
-);
-
-const EditButton = () => (
-  <Button size="small" color="primary">
-    <EditIcon />
-    Edit
-  </Button>
-);
-
-const DeleteButton = () => (
-  <Button size="small" color="primary">
-    <DeleteIcon />
-    Delete
-  </Button>
-);
-
-const SaveButton = () => (
-  <Button size="small" color="primary">
-    <SaveIcon />
-    Save
-  </Button>
-);
-
-type State = "editting";
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    // marginLeft: theme.spacing.unit, TODO
+    // marginRight: theme.spacing.unit, TODO
+    // marginLeft: 15,
+    // marginRight: 15,
+    width: 200
+  },
+  dense: {
+    marginTop: 19
+  },
+  menu: {
+    width: 200
+  }
+});
 
 const OmakeItemListItem: React.FC = () => {
+  const [isExpanding, setExpanding] = React.useState(false);
   const [isEditing, setEditing] = React.useState(false);
+  const classes = useStyles();
+
+  const onHandleExpand = (event: React.ChangeEvent<{}>, expanded: boolean) => {
+    if (isEditing) {
+      return;
+    }
+    setExpanding(expanded);
+  };
+
+  const onEditClicked = () => {
+    setEditing(true);
+  };
+
+  const onCancelClicked = () => {
+    setEditing(false);
+  };
 
   return (
     <OmakeItemListItemRoot>
-      <ExpansionPanel defaultExpanded={true} expanded={true}>
+      <ExpansionPanel expanded={isExpanding} onChange={onHandleExpand}>
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <TextField label={"title is here"} />
+          {isEditing ? (
+            <TextField value={"title is here"} />
+          ) : (
+            <InputBase value={"title is here"} readOnly={true} />
+          )}
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Grid container={true}>
-            <TextField label={"Description"} multiline={true} />
-          </Grid>
-
-          <Grid container={true}>
-            <TextField label={"file path"} />
+          <Grid container={true} alignItems={"center"}>
+            <Grid item={true}>
+              {isEditing ? (
+                <TextField
+                  id="standard-select-currency"
+                  select={true}
+                  className={classes.textField}
+                  value={`wav_24bit`}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu
+                    }
+                  }}
+                  margin="normal"
+                >
+                  {fileType.map(({ value, label }) => (
+                    <MenuItem key={value} value={value}>
+                      {label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              ) : (
+                <InputBase value={"wav_24bit"} readOnly={true} />
+              )}
+            </Grid>
+            <Grid item={true}>
+              {isEditing ? (
+                <TextField value={"temp_file_path"} />
+              ) : (
+                <InputBase value={"temp_file_path"} readOnly={true} />
+              )}
+            </Grid>
           </Grid>
         </ExpansionPanelDetails>
         <Divider />
         <ExpansionPanelActions>
           {isEditing ? (
             <React.Fragment>
-              <CancelButton />
+              <CancelButton onClick={onCancelClicked} />
               <SaveButton />
             </React.Fragment>
           ) : (
             <React.Fragment>
-              <EditButton />
+              <EditButton onClick={onEditClicked} />
               <DeleteButton />
             </React.Fragment>
           )}
@@ -120,6 +202,7 @@ const OmakeItemList: React.FC<OmakeItemListProps> = props => {
 
   return (
     <OmakeItemListRoot {...others}>
+      <AddItemButton />
       {items.map(({ key }) => (
         <OmakeItemListItem key={key} />
       ))}
