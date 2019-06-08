80249bf7867bf6c264d32c261e4cc313ac76f2e7
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2019-06-08 14:56:21 +0900

    replace download file logic

--- app/utils/network.ts
+++ app/utils/network.ts
@@ -4,16 +4,46 @@ export const downloadFromFirebaseStorage = async (
   storageUrl: string,
   originalName: string
 ): Promise<void> => {
-  const downloadURL = await storage()
+  const downloadUrl = await storage()
     .refFromURL(storageUrl)
     .getDownloadURL();
 
-  // TODO: CORSの対策
-  // ダウンロードではなくファイルページへの遷移になっている
-  const a = document.createElement("a");
-  a.download = originalName;
-  a.href = downloadURL;
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = "blob";
+
+  const onProgress = (event: ProgressEvent) => {
+    const { loaded, total, lengthComputable } = event;
+
+    if (lengthComputable) {
+      const percentComplete = (loaded / total) * 100;
+      // ...
+    } else {
+      // 全体の長さが不明なため、進捗情報を計算できない
+    }
+  };
+
+  const onLoad = (event: ProgressEvent) => {
+    const blob = xhr.response;
+    const a = document.createElement("a");
+    a.download = originalName;
+    a.href = window.URL.createObjectURL(blob);
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
+
+  const onError = (event: ProgressEvent) => {
+    //
+  };
+
+  const onAbort = (event: ProgressEvent) => {
+    //
+  };
+
+  xhr.addEventListener("progress", onProgress);
+  xhr.addEventListener("load", onLoad);
+  xhr.addEventListener("error", onError);
+  xhr.addEventListener("abort", onAbort);
+  xhr.open("GET", downloadUrl);
+  xhr.send();
 };
