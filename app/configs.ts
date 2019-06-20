0347ae883513a890c93a84a2a5d44e3e8fd50165
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2019-06-20 13:03:27 +0900

    edit configs and scripts to deploy pro env

--- app/configs.ts
+++ app/configs.ts
@@ -1,5 +1,6 @@
 export default {
   contactFormUrl:
     "https://docs.google.com/forms/d/e/1FAIpQLSe5bSPvJ5XQM0IACqZ9NKoHuRUAcC_V1an16JGwHh6HeGd-oQ/viewform?usp=pp_url&entry.326070868=DLCode",
-  twitterUrl: "https://twitter.com/T28_tatsuya"
+  twitterUrl: "https://twitter.com/T28_tatsuya",
+  firebaseConfigs
 };
