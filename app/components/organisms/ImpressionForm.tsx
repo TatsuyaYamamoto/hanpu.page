3a7e8715386b51a1159f633d4d575b16df9591f3
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2019-09-08 23:39:26 +0900

    feat: add form to post impression

---
+++ app/components/organisms/ImpressionForm.tsx
@@ -0,0 +1,94 @@
+import * as React from "react";
+const { useState, useMemo, useCallback } = React;
+
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  Paper,
+  TextField,
+  Typography
+} from "@material-ui/core";
+
+import useImpression from "../hooks/useImpression";
+
+interface ImpressionFormProps {
+  productId: string;
+}
+
+const ImpressionForm: React.FC<ImpressionFormProps> = props => {
+  const { productId } = props;
+
+  const [text, setText] = useState<string>("");
+
+  const [submitState, setSubmitState] = useState<
+    "waiting" | "sending" | "success"
+  >("waiting");
+
+  const { postImpression } = useImpression();
+
+  const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  }, []);
+
+  const canSubmit = useMemo(() => {
+    return text !== "" && submitState === "waiting";
+  }, [text, submitState]);
+
+  const openDialog = useMemo(() => {
+    return submitState === "success";
+  }, [submitState]);
+
+  const onSubmitClicked = useCallback(
+    (_: React.MouseEvent<HTMLButtonElement>) => {
+      if (!text) {
+        return;
+      }
+      setSubmitState("sending");
+
+      postImpression(productId, text).then(() => {
+        setSubmitState("success");
+      });
+    },
+    [productId, text]
+  );
+
+  const handleCloseDialog = useCallback(
+    (_: React.MouseEvent<HTMLButtonElement>) => {
+      setSubmitState("waiting");
+    },
+    [productId, text]
+  );
+
+  return (
+    <>
+      <Paper>
+        <Typography>ご感想はこちらへ！</Typography>
+        <TextField
+          multiline={true}
+          margin="normal"
+          variant="outlined"
+          fullWidth={true}
+          value={text}
+          onChange={onChange}
+        />
+        <Button disabled={!canSubmit} onClick={onSubmitClicked}>
+          送信
+        </Button>
+      </Paper>
+
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle>ありがとうございました（・８・）</DialogTitle>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default ImpressionForm;
+export { ImpressionFormProps };
