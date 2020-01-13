3ba60637120ba961541ec06addae96c4fbb87c04
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2020-01-13 19:22:16 +0900

    wip: set fatal error handling

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-const { useEffect } = React;
+const { useEffect, useCallback } = React;
 
 import { AppProps } from "next/app";
 import Head from "next/head";
@@ -24,12 +24,53 @@ body {
 const MyApp: React.FC<AppProps> = props => {
   const { Component, pageProps } = props;
   const router = useRouter();
-  const { init: initGa, logPageView } = useGa();
+  const { init: initGa, logPageView, logError } = useGa();
+
+  const requestErrorDetailContact = useCallback(
+    (errorDetail: any) => {
+      const { userAgent, language } = navigator;
+      const { location } = window;
+
+      const info = {
+        error: errorDetail,
+        userAgent,
+        language,
+        location
+      };
+
+      logError(JSON.stringify(info), true);
+
+      if (
+        confirm(
+          "予期せぬエラーが発生してしまいました。\n\n恐れ入りますが、問い合わせフォームを起動してエラーの詳細を送信して頂けませんでしょうか？(エラーの情報は自動で入力されます。)"
+        )
+      ) {
+        const detail = `${btoa(
+          unescape(encodeURIComponent(JSON.stringify(info)))
+        )}`;
+        const contactFormUrl = `https://docs.google.com/forms/d/e/1FAIpQLSe5bSPvJ5XQM0IACqZ9NKoHuRUAcC_V1an16JGwHh6HeGd-oQ/viewform?usp=pp_url&entry.1991364045=%E4%B8%8D%E5%85%B7%E5%90%88%E5%A0%B1%E5%91%8A...+/+Bug+Report&entry.326070868=DLCode&entry.1884055698=${detail}`;
+        window.location.href = contactFormUrl;
+      } else {
+        // tslint:disable-next-line:no-console
+        console.error(info);
+      }
+    },
+    [logError]
+  );
 
   useEffect(() => {
     initGa();
     logPageView();
     router.events.on("routeChangeComplete", logPageView);
+
+    window.onerror = (message, file, lineNo, colNo, error) => {
+      const errorDetail = { message, file, lineNo, colNo, error };
+      requestErrorDetailContact(errorDetail);
+    };
+
+    window.addEventListener("unhandledrejection", e => {
+      requestErrorDetailContact(e.reason);
+    });
   }, []);
 
   return (
