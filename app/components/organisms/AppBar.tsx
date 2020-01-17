f3f5a0b1443fa350d0e8c5fe036176456655f90d
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2020-01-17 22:26:43 +0900

    fix: TODO, remove useless code

--- app/components/organisms/AppBar.tsx
+++ app/components/organisms/AppBar.tsx
@@ -1,7 +1,4 @@
 import * as React from "react";
-const { useState } = React;
-
-import { useRouter } from "next/router";
 
 import {
   default as MuiAppBar,
@@ -11,15 +8,11 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import Icon from "@material-ui/core/Icon";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 
 import styled from "styled-components";
 
 import FlexSpace from "../atoms/FlexSpace";
 import Logo from "../atoms/Logo";
-
-import useAuthSession from "../hooks/useAuthSession";
 
 const StyledMuiAppBar = styled(MuiAppBar as React.FC<MuiAppBarProps>)`
   && {
@@ -28,56 +21,6 @@ const StyledMuiAppBar = styled(MuiAppBar as React.FC<MuiAppBarProps>)`
     z-index: 99;
   }
 `;
-
-// @ts-ignore
-const AppBarMenu = props => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const openMenu = Boolean(anchorEl);
-  const { logout, isLoggedIn } = useAuthSession();
-  const router = useRouter();
-
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(anchorEl ? null : event.currentTarget);
-  };
-
-  const handleLogout = () => {
-    logout();
-  };
-
-  const handleSettings = () => {
-    router.push(`/settings`);
-  };
-
-  return (
-    <>
-      <IconButton
-        aria-owns={openMenu ? "menu-appbar" : undefined}
-        aria-haspopup="true"
-        onClick={handleMenu}
-        color="inherit"
-      >
-        <Icon>more_vert</Icon>
-      </IconButton>
-      <Menu
-        id="menu-appbar"
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "right"
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "right"
-        }}
-        open={openMenu}
-        onClose={handleMenu}
-      >
-        {isLoggedIn && <MenuItem onClick={handleLogout}>ログアウト</MenuItem>}
-        <MenuItem onClick={handleSettings}>設定</MenuItem>
-      </Menu>
-    </>
-  );
-};
 
 interface AppBarProps {
   onBack?: () => void;
@@ -100,10 +43,7 @@ const AppBar: React.FC<AppBarProps> = ({ onBack }) => {
       <StyledMuiAppBar position="static">
         <Toolbar>
           {onBack ? back : logo}
-
           <FlexSpace />
-          {/* TODO: Show help! */}
-          {/*<AppBarMenu />*/}
         </Toolbar>
       </StyledMuiAppBar>
     </>
