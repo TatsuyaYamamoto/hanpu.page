69cda9874e9e99f62b5e7df9783ebd448a402f50
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2020-01-31 00:46:11 +0900

    feat: show user icon on AppBar and show logout button

--- app/components/organisms/AppBar.tsx
+++ app/components/organisms/AppBar.tsx
@@ -8,11 +8,15 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import Icon from "@material-ui/core/Icon";
+import Avatar from "@material-ui/core/Avatar";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 import styled from "styled-components";
 
 import FlexSpace from "../atoms/FlexSpace";
 import Logo from "../atoms/Logo";
+import useFirebase from "../hooks/useFirebase";
 
 const StyledMuiAppBar = styled(MuiAppBar as React.FC<MuiAppBarProps>)`
   && {
@@ -27,6 +31,10 @@ interface AppBarProps {
 }
 
 const AppBar: React.FC<AppBarProps> = ({ onBack }) => {
+  const { user, logout } = useFirebase();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
   const back = (
     <IconButton color="inherit" onClick={onBack}>
       <Icon>arrow_back</Icon>
@@ -38,12 +46,58 @@ const AppBar: React.FC<AppBarProps> = ({ onBack }) => {
     </Typography>
   );
 
+  const handleMenu = (event?: any) => {
+    setAnchorEl(open ? null : event.currentTarget);
+  };
+
+  const menus = [
+    {
+      label: "ログアウト",
+      onClick: () => {
+        handleMenu();
+        logout();
+      }
+    }
+  ];
+
+  const userIcon = user ? (
+    <>
+      <IconButton onClick={handleMenu}>
+        <Avatar src={user && user.photoURL ? user.photoURL : ""} />
+      </IconButton>
+      <Menu
+        keepMounted={true}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleMenu}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center"
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center"
+        }}
+      >
+        {menus.map(menu => (
+          <MenuItem key={menu.label} onClick={menu.onClick}>
+            {menu.label}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  ) : (
+    <></>
+  );
+
   return (
     <>
       <StyledMuiAppBar position="static">
         <Toolbar>
           {onBack ? back : logo}
           <FlexSpace />
+          {userIcon}
         </Toolbar>
       </StyledMuiAppBar>
     </>
