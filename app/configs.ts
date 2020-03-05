fae01c5ce446a692b11b9f34cccaf71a666f7d50
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2020-03-05 17:31:53 +0900

    fix: handle production flag logic

--- app/configs.ts
+++ app/configs.ts
@@ -1,39 +1,38 @@
+const isProduction = process.env.GCLOUD_PROJECT === "dl-code";
+
 export default {
   contactFormUrl:
     "https://docs.google.com/forms/d/e/1FAIpQLSe5bSPvJ5XQM0IACqZ9NKoHuRUAcC_V1an16JGwHh6HeGd-oQ/viewform?usp=pp_url&entry.326070868=DLCode",
   twitterUrl: "https://twitter.com/T28_tatsuya",
-  firebaseConfigs:
-    process.env.NODE_ENV === "production"
-      ? {
-          apiKey: "AIzaSyCooAMMW0UzfXJln2JUHkKIv8Va4tzLUt0",
-          authDomain: "dl-code.firebaseapp.com",
-          databaseURL: "https://dl-code.firebaseio.com",
-          projectId: "dl-code",
-          storageBucket: "dl-code.appspot.com",
-          messagingSenderId: "60887072982",
-          appId: "1:60887072982:web:174e519749625525"
-        }
-      : {
-          apiKey: "AIzaSyDkyIH-immHfoQY59kbEfWi9T1npPTUv0k",
-          authDomain: "dl-code-dev.firebaseapp.com",
-          databaseURL: "https://dl-code-dev.firebaseio.com",
-          projectId: "dl-code-dev",
-          storageBucket: "dl-code-dev.appspot.com",
-          messagingSenderId: "170382784624",
-          appId: "1:170382784624:web:42b794526ad81a74"
-        },
-  apiServerOrigin:
-    process.env.NODE_ENV === "production"
-      ? "https://api.sokontokoro-factory.net"
-      : "https://api-dev.sokontokoro-factory.net",
-  auth0:
-    process.env.NODE_ENV === "production"
-      ? {
-          domain: "",
-          clientId: ""
-        }
-      : {
-          domain: "sokontokoro-factory-dev.auth0.com",
-          clientId: "iJXXm5eUjZuGX0AOITjyWGPgtbpjZnjk"
-        }
+  firebaseConfigs: isProduction
+    ? {
+        apiKey: "AIzaSyCooAMMW0UzfXJln2JUHkKIv8Va4tzLUt0",
+        authDomain: "dl-code.firebaseapp.com",
+        databaseURL: "https://dl-code.firebaseio.com",
+        projectId: "dl-code",
+        storageBucket: "dl-code.appspot.com",
+        messagingSenderId: "60887072982",
+        appId: "1:60887072982:web:174e519749625525"
+      }
+    : {
+        apiKey: "AIzaSyDkyIH-immHfoQY59kbEfWi9T1npPTUv0k",
+        authDomain: "dl-code-dev.firebaseapp.com",
+        databaseURL: "https://dl-code-dev.firebaseio.com",
+        projectId: "dl-code-dev",
+        storageBucket: "dl-code-dev.appspot.com",
+        messagingSenderId: "170382784624",
+        appId: "1:170382784624:web:42b794526ad81a74"
+      },
+  apiServerOrigin: isProduction
+    ? "https://api.sokontokoro-factory.net"
+    : "https://api-dev.sokontokoro-factory.net",
+  auth0: isProduction
+    ? {
+        domain: "",
+        clientId: ""
+      }
+    : {
+        domain: "sokontokoro-factory-dev.auth0.com",
+        clientId: "iJXXm5eUjZuGX0AOITjyWGPgtbpjZnjk"
+      }
 };
