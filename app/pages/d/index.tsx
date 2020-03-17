6d04759e0edd3a14734a9a320b791951056812ed
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2020-03-17 11:53:32 +0900

    fix: return 404 in requesting to /d/ next.js don't support trailing slash.

--- app/pages/d/index.tsx
+++ app/pages/d/index.tsx
@@ -1,16 +1,20 @@
+/**
+ * @fileOverview
+ * DL-QRCodeにエンコードするためのための短縮パスを定義する{@link NextPage}.
+ * queryはそのまま、/download/verifyへredirectする。
+ *
+ * !!!!!!IMPORTANT!!!!!!
+ * next.js(v9.1.7)はtrailing slashに対応していないため、hostingでredirectの設定を行っている。
+ * firebase.jsonを参照。
+ *
+ * QueryParameters:
+ *  - Key: c
+ *    Description: ダウンロードコードのテキストフィールドに値を入力した状態で表示する
+ */
 import { default as React } from "react";
 import { NextPage } from "next";
 
 import { stringify } from "querystring";
-
-/**
- * QueryParameters:
- *  - Key: c
- *    Description: ダウンロードコードのテキストフィールドに値を入力した状態で表示する
- *
- * @param props
- * @constructor
- */
 
 const DPage: NextPage = () => {
   return <></>;
