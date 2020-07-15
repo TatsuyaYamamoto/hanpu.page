62bdc96d2cb57ddd72f14060a5115de525b217b7
Tatsuya Yamamoto <t28.is.tatsuya@gmail.com>
2020-07-15 12:23:32 +0900

    fix: replace firestore sdk to export

--- app/utils/gcp.ts
+++ app/utils/gcp.ts
@@ -1,37 +1,28 @@
-import { firestore_v1, google } from "googleapis";
-
-import { format as dateFormat } from "date-fns";
-
-import ExportDocuments = firestore_v1.Params$Resource$Projects$Databases$Exportdocuments;
-import Firestore = firestore_v1.Firestore;
+// 型定義がconflictするので、requireを使う
+// node_modules/firebase-admin/node_modules/@google-cloud/firestore/types/firestore.d.ts(23,1): error TS6200: Definitions of the following identifiers conflict with those in another file: DocumentData, UpdateData, Firestore, GeoPoint, Transaction, WriteBatch, WriteResult, DocumentReference, DocumentSnapshot, QueryDocumentSnapshot, OrderByDirection, WhereFilterOp, Query, QuerySnapshot, DocumentChangeType, CollectionReference, FieldValue, FieldPath, Timestamp, v1beta1, v1, OK, CANCELLED, UNKNOWN, INVALID_ARGUMENT, DEADLINE_EXCEEDED, NOT_FOUND, ALREADY_EXISTS, PERMISSION_DENIED, RESOURCE_EXHAUSTED, FAILED_PRECONDITION, ABORTED, OUT_OF_RANGE, UNIMPLEMENTED, INTERNAL, UNAVAILABLE, DATA_LOSS, UNAUTHENTICATED, FirebaseFirestore
+// tslint:disable-next-line:no-var-requires
+const firestore = require("@google-cloud/firestore");
+import moment from "moment-timezone";
 
 import { projectId } from "../functions/processEnv";
 
-const backupBucket = `gs://${projectId}.appspot.com`;
-const exportCollections = ["auditLogs", "downloadCodeSets", "products"];
-
+/**
+ * @see https://firebase.google.com/docs/firestore/solutions/schedule-export?hl=ja
+ */
 export const backupFirestoreData = async () => {
-  const timestamp = dateFormat(new Date(), "yyyy-mm-dd_hh-MM");
+  const timestamp = moment.tz("Asia/Tokyo").format("YYYY-MM-DD_HH-mm");
+  const firestoreClient = new firestore.v1.FirestoreAdminClient();
 
-  // https://cloud.google.com/functions/docs/concepts/services#using_services_with_cloud_functions
-  const auth = await google.auth.getClient({
-    projectId,
-    scopes: [
-      "https://www.googleapis.com/auth/datastore",
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
+  // https://googleapis.dev/nodejs/firestore/latest/v1.FirestoreAdminClient.html#databasePath
+  const databaseName = firestoreClient.databasePath(projectId, "(default)");
+  const backupBucket = `gs://${projectId}.appspot.com`;
+  const outputUriPrefix = `${backupBucket}/backups/${timestamp}`;
+  const exportCollections = ["auditLogs", "downloadCodeSets", "products"];
+
+  // https://googleapis.dev/nodejs/firestore/latest/v1.FirestoreAdminClient.html#exportDocuments
+  return firestoreClient.exportDocuments({
+    name: databaseName,
+    outputUriPrefix,
+    collectionIds: exportCollections
   });
-
-  const params: ExportDocuments = {
-    auth,
-    name: `projects/${projectId}/databases/(default)`,
-    requestBody: {
-      collectionIds: exportCollections,
-      outputUriPrefix: `${backupBucket}/backups/${timestamp}`
-    }
-  };
-
-  const firestore = new Firestore({});
-
-  return firestore.projects.databases.exportDocuments(params);
 };
