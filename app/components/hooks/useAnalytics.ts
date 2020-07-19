396f4d6a153a65a1b64e7b5475520bf8b5c9ac7c
Tatsuya Yamamoto <t28.is.tatsuya@gmail.com>
2020-07-19 23:13:30 +0900

    feat: show graph of activate acount

---
+++ app/components/hooks/useAnalytics.ts
@@ -0,0 +1,39 @@
+import { firestore } from "firebase/app";
+
+import {
+  AnalyticsDocument,
+  AnalyticsType,
+  CollectionPath as AnalyticsCollectionPath
+} from "../../domains/Analytics";
+import useFirebase from "./useFirebase";
+
+// const log = (message?: any, ...optionalParams: any[]): void => {
+//   // tslint:disable-next-line
+//   console.log(`[useAnalytics] ${message}`, ...optionalParams);
+// };
+
+const useAnalytics = () => {
+  const { user } = useFirebase();
+
+  // prettier-ignore
+  const analyticsColRef = firestore().collection(AnalyticsCollectionPath) as firestore.CollectionReference<AnalyticsDocument>;
+
+  const loadSimpleActivateCount = async () => {
+    if (!user) {
+      return;
+    }
+
+    const snap = await analyticsColRef
+      .where("type", "==", AnalyticsType.SIMPLE_ACTIVATE_COUNT)
+      .where("ownerUid", "==", user.uid)
+      .get();
+
+    return snap.docs.map(doc => doc.data());
+  };
+
+  return {
+    loadSimpleActivateCount
+  };
+};
+
+export default useAnalytics;
--- app/components/organisms/UsageGraph/UsageGraph.tsx
+++ app/components/organisms/UsageGraph/UsageGraph.tsx
@@ -1,15 +1,19 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import styled from "styled-components";
 
-import { VictoryBar, VictoryChart, VictoryTheme, VictoryAxis } from "victory";
+import {
+  VictoryLine,
+  VictoryChart,
+  VictoryTheme,
+  VictoryBrushContainer,
+  VictoryAxis,
+  VictoryZoomContainer,
+  DomainTuple
+} from "victory";
 import { Paper, Tabs, Tab } from "@material-ui/core";
+import { useDebouncedCallback } from "use-debounce";
 
-const data = [
-  { quarter: 1, earnings: 13000 },
-  { quarter: 2, earnings: 16500 },
-  { quarter: 3, earnings: 14250 },
-  { quarter: 4, earnings: 19000 }
-];
+import useAnalytics from "../../hooks/useAnalytics";
 
 const ChartRoot = styled.div`
   svg {
@@ -19,7 +23,70 @@ const ChartRoot = styled.div`
   }
 `;
 
+const zoomableGraphWidth = 550;
+const zoomableGraphHeight = 300;
+const fullScaleGraphWidth = zoomableGraphWidth;
+const fullScaleGraphHeight = 100;
+
+interface VisibleData {
+  timeline: { x: Date; y: number }[];
+  cumulative: { x: Date; y: number }[];
+}
+
 const UsageGraph: FC = () => {
+  const analytics = useAnalytics();
+  const [total, setTotal] = useState("");
+  const [visibleData, setVisibleData] = useState<VisibleData>({
+    timeline: [],
+    cumulative: []
+  });
+
+  useEffect(() => {
+    (async () => {
+      const simpleActivateCount = await analytics.loadSimpleActivateCount();
+      if (simpleActivateCount) {
+        const {
+          total: rowTimeline,
+          timeline: rowTimelineDatumMap
+        } = simpleActivateCount[0].data;
+        const rowTimelineData = Object.keys(rowTimelineDatumMap)
+          .map(date => {
+            return {
+              x: date,
+              y: rowTimelineDatumMap[date]
+            };
+          })
+          .sort((a, b) => (a.x < b.x ? -1 : 1));
+
+        const timelineData: { x: Date; y: number }[] = [];
+        const cumulativeData: { x: Date; y: number }[] = [];
+
+        rowTimelineData.forEach(({ x, y }, index) => {
+          const prevCount = cumulativeData[index - 1]?.y ?? 0;
+
+          timelineData.push({ x: new Date(x), y });
+          cumulativeData.push({ x: new Date(x), y: y + prevCount });
+        });
+
+        setVisibleData({
+          timeline: timelineData,
+          cumulative: cumulativeData
+        });
+        setTotal(rowTimeline.toString(10));
+      }
+    })();
+  }, []);
+
+  const [zoomDomain, setZoomDomain] = useState<
+    { x?: DomainTuple; y: DomainTuple } | undefined
+  >(undefined);
+  const [handleBrush] = useDebouncedCallback(
+    (domain: { x?: DomainTuple; y: DomainTuple }) => {
+      setZoomDomain(domain);
+    },
+    500
+  );
+
   return (
     <Paper>
       <Tabs
@@ -28,26 +95,72 @@ const UsageGraph: FC = () => {
         textColor="primary"
         aria-label="disabled tabs example"
       >
-        <Tab label="総ダウンロード数" />
+        <Tab label={`アクティベーション数 (${total})`} />
       </Tabs>
 
       <ChartRoot>
         <VictoryChart
-          // height={333}
-          // domainPadding will add space to each side of VictoryBar to
-          // prevent it from overlapping the axis
-          // domainPadding={20}
+          scale={{ x: "time" }}
+          width={zoomableGraphWidth}
+          height={zoomableGraphHeight}
           theme={VictoryTheme.material}
+          containerComponent={
+            <VictoryZoomContainer
+              responsive={false}
+              zoomDimension="x"
+              zoomDomain={zoomDomain}
+            />
+          }
         >
-          <VictoryBar data={data} x="quarter" y="earnings" />
-          <VictoryAxis
-            // tickValues specifies both the number of ticks and where
-            // they are placed on the axis
-            tickValues={[1, 2, 3, 4]}
-            tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+          <VictoryLine
+            data={visibleData.timeline}
+            style={{
+              data: { stroke: "tomato" }
+            }}
           />
-          <VictoryAxis dependentAxis={true} />
-          <VictoryBar data={data} x="quarter" y="earnings" />
+          <VictoryLine
+            data={visibleData.cumulative}
+            style={{
+              data: { stroke: "blue" }
+            }}
+          />
+        </VictoryChart>
+        <VictoryChart
+          width={fullScaleGraphWidth}
+          height={fullScaleGraphHeight}
+          scale={{ x: "time" }}
+          padding={{ top: 0, left: 50, right: 50, bottom: 30 }}
+          containerComponent={
+            <VictoryBrushContainer
+              responsive={false}
+              brushDimension="x"
+              // @ts-ignore
+              onBrushDomainChange={handleBrush}
+            />
+          }
+        >
+          <VictoryAxis
+            tickValues={[
+              new Date(2019, 1, 1),
+              new Date(2020, 1, 1),
+              new Date(2021, 1, 1)
+            ]}
+            // TODO
+            // tslint:disable-next-line
+            tickFormat={x => new Date(x).getFullYear()}
+          />
+          <VictoryLine
+            style={{
+              data: { stroke: "tomato" }
+            }}
+            data={visibleData.timeline}
+          />
+          <VictoryLine
+            style={{
+              data: { stroke: "blue" }
+            }}
+            data={visibleData.cumulative}
+          />
         </VictoryChart>
       </ChartRoot>
     </Paper>
